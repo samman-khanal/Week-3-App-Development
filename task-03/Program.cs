ccd2bd4c3101467042cae185fd565cf194546993
samman-khanal <samman.samman345@gmail.com>
2025-11-11 14:22:58 +0545

    Task Three

---
+++ task-03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter the age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age < 13)
+{
+    Console.WriteLine("Child");
+}
+else if (age >= 13 && age <= 19)
+{
+    Console.WriteLine("Teenager");
+}
+else
+{
+    Console.WriteLine("Adult");
+}
