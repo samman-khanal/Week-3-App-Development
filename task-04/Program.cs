17a6b271836e940cb2ffd46f72b76e429ecfc583
samman-khanal <samman.samman345@gmail.com>
2025-11-11 14:26:31 +0545

    Task Four

---
+++ task-04/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number between 1 to 7: ");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid input! Please enter a number between 1 to 7.");
+                break;
+        }
+    }
+}
